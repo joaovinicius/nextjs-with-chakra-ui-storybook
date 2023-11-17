78b1d3700e4f32ede3bb75cfb741b2673fca1fc9
GUIFRE88 <guilherme.fre073@gmail.com>
2023-11-17 13:09:07 -0300

    Commit inicial

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import styles from '../page.module.css'
+import AtomButton from '@/components/Atoms/Button'
+
+export default function Login() {
+  return (
+    <p color='primary'>dasdas</p>
+  )
+}
